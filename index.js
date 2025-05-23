8596bb1f3b9849162c406afc1860ca57ad0447af
Ubuntu <ubuntu@ip-172-31-44-164.eu-north-1.compute.internal>
2025-05-23 20:57:28 +0000

    changes

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 require('dotenv').config
 const express = require('express')
 const app = express()
-const port = 4000
+const port = 4000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello my name is nikhil this is my first deployed app I am from jhajha Bihar')
 })
 
 app.get('/login', (req,res) => {
     res.send('Welcome Nikhil')
 })
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening on port ${port}`)
 })
