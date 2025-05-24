0a593d78b542468cec6c18c389a4d78b2baa13c2
kmrr-nikhil <nikhil4u43@gmail.com>
2025-05-24 18:19:01 +0530

    added require path cpnst

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config
 const express = require('express')
 const app = express()
 const port = 4000;
+const path=require('path')
 
 app.get('/', (req, res) => {
   res.send('Hello my name is nikhil this is my first deployed app I am from jhajha Bihar I am working in Pune')
